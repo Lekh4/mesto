4975ba7f3ef26152d459686435548d18d4f045c7
Aleksey Maslanov <aleks.maslanov@gmail.com>
2022-07-10 05:46:14 +0300

    pw4

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const popupEditAuthorBtn = document.querySelector('.profile__edit-button');
+const popupCloseBtn = document.querySelector('.popup__close-button');
+const formAuthor = document.querySelector('.popup__edit-name');
+
+const authorNameInForm = document.querySelector('.form__input_info_author-name');
+const authorProfile = document.querySelector('.profile__name');
+const authorProfInform = document.querySelector('.form__input_info_author-name-profession');
+const authorProfProfile = document.querySelector('.profile__profession');
+
+function openPopup(element) {
+  element.classList.add('popup__visible');
+};
+
+function closePopup(element) {
+  element.classList.remove('popup__visible');
+};
+
+function openProfilePopup() {
+  authorNameInForm.value = authorProfile.textContent;
+  authorProfInform.value = authorProfProfile.textContent;
+  openPopup(formAuthor);
+};
+
+function submit(evt) {
+  evt.preventDefault();
+  authorProfile.textContent = authorNameInForm.value;
+  authorProfProfile.textContent = authorProfInform.value;
+  closePopup(formAuthor);
+};
+
+popupEditAuthorBtn.addEventListener('click', openProfilePopup);
+popupCloseBtn.addEventListener('click', () => closePopup(formAuthor));
+formAuthor.addEventListener('submit', submit);
